80d442e3fce25f9973ef065add3eb66d3ce60f08
anadcruz <109186269+anadcruz@users.noreply.github.com>
2024-06-09 07:31:01 -0300

    Update salvando_alteracoes_no_repositorio_local.py

--- salvando_alteracoes_no_repositorio_local.py
+++ salvando_alteracoes_no_repositorio_local.py
@@ -1,1 +1,16 @@
 git status //comando para ver o status da nossa árvore de trabalho ou preparação "stage area"
+git init
+git commit -m"commit inicial" //"commita"
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo nomeArquivo/ > .gitignore // comando para o git ignorar certo arquivo 
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+git add . ou git add nomeArquivo //comando para eu adicionar determinar arquivo ou todos os arquivos para a stage area
+git commit -m"Adicionar arquivo gitignore"
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+git log //mostra o histório dos commits feitos 
